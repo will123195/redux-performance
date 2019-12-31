406dbc08856ebe955cc9f138c5864e6763d96651
will123195 <will123195@gmail.com>
2019-12-30 23:12:58 -0700

    build

---
+++ build/precache-manifest.bcb77de6bcc581c07f68ec75bb52ad02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4035635b4f5e6cb644c24c4356b5d9f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4acd2bb61f08536e0e11",
+    "url": "/static/js/2.0823fbd1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.0823fbd1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47116769582e84018159",
+    "url": "/static/js/main.75acd808.chunk.js"
+  },
+  {
+    "revision": "3e7cd734131767a1770c",
+    "url": "/static/js/runtime-main.5ee1c62c.js"
+  }
+]);
